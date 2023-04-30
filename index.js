5bd84fccdcffbbf7aa7ae82cfcc30f523e616d6a
thediep094 <thediep094@gmail.com>
2023-05-01 02:28:40 +0700

    dsadsa

--- index.js
+++ index.js
@@ -59,3 +59,20 @@ io.on("connection", (socket) => {
         console.log("User Disconnected", socket.id);
     });
 });
+
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://league-of-legend.vercel.app",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization",
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
