e0725d85a8e97ec15475b42479392d6d5f1bc745
thediep094 <thediep094@gmail.com>
2023-05-01 13:36:22 +0700

    đsadá

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const server = app.listen(port, () => {
 
 const io = new Server(server, {
     cors: {
-        origin: true,
+        origin: "https://league-of-legend.vercel.app",
     },
 });
 io.on("connection", (socket) => {
